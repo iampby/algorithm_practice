6136a34af34a3e20e136d3aae48362f7fac79620
iampby <2567469480@qq.com>
2020-10-06 18:11:03 +0800

    实现了迭代版的AVL树，继承于二叉搜索树，二叉搜索树采用3+4重构方法对树进行zig、zag旋转，树区分了拷贝构造和移动构造，赋值操作符参数采用调用构造函数的方式实现构造赋值和移动赋值的二合一

---
+++ AVL/AVL.h
@@ -0,0 +1,152 @@
+#ifndef AVL_H_H
+#define AVL_H_H
+#include"BinSearchTree.h"
+namespace N_AVL{
+	using namespace BST;//使用二叉搜索树的命名空间
+	template<typename T>
+	class AVL : public BinSearchTree<T> {
+	
+	public:
+		AVL() {};
+		inline AVL(AVL<T>const&);
+		inline AVL(AVL<T>&&)TREE_NOTHROW;
+		inline AVL&operator=(AVL<T>)&;
+		~AVL() {};
+		virtual	BNP(T) insert(T const&)override;
+		virtual	bool remove(T const &)override;
+		virtual BNP(T) search(T const&d)override {
+		return BinSearchTree<T>::search(d);
+	};
+	//判断是否是理想平衡条件
+		 bool balanced(BNP(T)n) {
+		 return Stature(n->left) == Stature(n->right);
+	};
+	 //平衡因子获取
+		 int balancedFactor(BNP(T)n) {
+	 return Stature(n->left)- Stature(n->right);
+	 };
+	  //AVL是否平衡
+		 bool AVLBalanced(BNP(T)n) {
+		  return -2 < balancedFactor(n) && 2 > balancedFactor(n);
+	  }
+	  //获取较高的孩子，如果等高则返回和父亲同向的孩子
+		 BNP(T)&tallerChild(BNP(T)n) {
+		  return Stature(n->left) > Stature(n->right) ? n->left :
+			  (Stature(n->left) < Stature(n->right)) ? n->right :
+			  (n->isLeftChild()) ? n->left : n->right;
+	  }
+	};
+	template<typename T>
+	TREE_OUTOFLINE AVL<T>::AVL(AVL<T> const &avl)
+	{
+		BinSearchTree<T>*tp = static_cast<BinSearchTree<T>*>(this);
+		*tp = *static_cast<BinSearchTree<T>*>(const_cast<AVL<T>*>(&avl));//去掉const后调用基类的赋值函数
+	}
+
+	template<typename T>
+	TREE_OUTOFLINE AVL<T>::AVL(AVL<T>&&avl)TREE_NOTHROW
+	{
+		BinSearchTree<T>*tp = static_cast<BinSearchTree<T>*>(this);
+		*tp = std::move(*static_cast<BinSearchTree<T>*>(&avl));//调用基类的赋值函数
+	}
+
+	template<typename T>
+	TREE_OUTOFLINE AVL<T> & AVL<T>::operator=(AVL<T> avl) &
+	{
+		// TODO: 在此处插入 return 语句
+		BinSearchTree<T>*tp = static_cast<BinSearchTree<T>*>(this);
+		*tp = std::move(*static_cast<BinSearchTree<T>*>(&avl));//调用基类的赋值函数
+		return *this;
+	}
+	template<typename T>
+	TREE_OUTOFLINE BNP(T) AVL<T>::insert(T const &d)
+	{ 
+		BNP(T) p = search(d);
+		BNP(T)parent = nullptr;//父亲节点初始化0
+		if (p) {//如果已存在，让他作为后继插入或者最后一个值为d的后继
+			BNP(T)forerunner = p;
+			bool isLeft = false;
+			while (true)
+			{
+				if (forerunner->data == d) {
+					if (forerunner->hasRightChild()) {
+						forerunner = forerunner->right;//后继必然位于右边
+					}
+					else {
+						parent = forerunner;//找到父节点
+						isLeft = false;
+						break;
+					}
+				}
+				else {
+					if (forerunner->hasLeftChild())
+						forerunner = forerunner->left;//后继必在左孩子方向上
+					else {
+						parent = forerunner;//找到父节点
+						isLeft = true;
+						break;
+					}
+				}
+			}
+			if (parent) {//如果p有右孩子
+				++BinTree<T>::m_size;
+				BNP(T)n = new BinTreeNode<T>(d, parent);//新建一个节点 指向父亲节点
+				if (isLeft) {
+					parent->left = n;//和当点节点建立联系
+				}
+				else parent->right = n;
+				BinTree<T>::updateAncestorm_height(parent);//更新高度
+				p = n;
+			}
+			else {
+				++BinTree<T>::m_size;
+				BNP(T)n = new BinTreeNode<T>(d, p);//新建一个节点 指向父亲节点
+				p->right = n;
+				BinTree<T>::updateAncestorm_height(p);//更新高度
+				parent = p;
+				p = n;
+			}
+		}
+		else {
+			++BinTree<T>::m_size;
+			parent = this->m_hitNodeParent;
+			p = new BinTreeNode<T>(d, parent);
+			if (parent) {
+				if (parent->data > d)parent->left = p;
+				else parent->right = p;//重建关系 
+				BinTree<T>::updateAncestorm_height(parent);//更新高度
+			}
+			if (p->isRoot())this -> m_root = p;
+		}
+		//至多只需一次调整,如果没有发生调整，则时间复杂度为O（1）,即正比与树高
+		for(BNP(T)g=parent;g;g=g->parent)
+			if (!AVLBalanced(g)) {//一旦失衡，对孙子v节点做旋转操作 即对g节点采用3+4重构算法
+				this->rotateAt(tallerChild(tallerChild(g)));
+				break;//
+			}
+			else {
+					this->updatem_height(g);//即使平衡高度也可能变化
+			}
+		return p;
+	}
+
+	template<typename T>
+	TREE_OUTOFLINE bool AVL<T>::remove(T const &d)
+	{
+		if (!BinSearchTree<T>::remove(d))return false;//删除失败
+			//删除会发生“失衡传播”,需要多次调整,时间复杂度为与h正比
+		for (BNP(T)g = this->m_hitNodeParent; g; g = g->parent)
+			if (!AVLBalanced(g)) {//一旦失衡，对孙子v节点做旋转操作 即对g节点采用3+4重构算法
+				g  = this->rotateAt(tallerChild(tallerChild(g)));//
+		        int h = g->m_height;
+				BinSearchTree<T>::updatem_height(g);//即使平衡高度也可能变化
+				}
+		return true;
+	}
+
+}
+
+#endif // !AVL_H_H
+
+
+
---
+++ AVL/main.cpp
@@ -0,0 +1,286 @@
+﻿// AVL.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include"AVL.h"
+#include <iostream>
+#include<string>
+using namespace std;
+using namespace N_AVL;
+//测试全为固定生成树
+void testBTN();//树节点功能测试 size zig zag
+void testTraversal();//二叉树遍历测试
+void testBST();//测试二叉搜索树的插入删除功能
+void testAVL();//测试AVL功能
+int main()
+{
+	//testBTN();
+	//testTraversal();
+	//testBST();
+	testAVL();
+}
+void testAVL() {
+	AVL<string>tree;
+	tree.insert("i"); 
+	tree.insert( "d");
+	tree.insert( "c");
+	 tree.insert( "a");
+	tree.insert( "b");
+	 tree.insert("h");
+	tree.insert( "f");
+	tree.insert( "g");
+	tree.insert("e");
+	tree.insert( "l");
+	 tree.insert( "k");
+	tree.insert( "j");
+	 tree.insert( "n");
+	tree.insert( "m");
+	tree.insert( "p");
+	tree.insert( "o");
+	tree.insert("o");
+	tree.insert("q");
+	tree.insert("r");
+	tree.insert("s");
+	tree.insert("u");
+	tree.insert("x");
+	tree.insert("y");
+	tree.insert("v");
+	tree.insert("t");
+	tree.insert("z");
+	tree.insert("w");
+	tree.insert("I");
+	tree.insert("D");
+	tree.insert("C");
+	tree.insert("A");
+	tree.insert("B");
+	tree.insert("H");
+	tree.insert("F");
+	tree.insert("G");
+	tree.insert("E");
+	tree.insert("L");
+	tree.insert("K");
+	tree.insert("J");
+	tree.insert("N");
+	tree.insert("M");
+	tree.insert("P");
+	tree.insert("O");
+	tree.insert("Q");
+	tree.insert("R");
+	tree.insert("S");
+	tree.insert("U");
+	tree.insert("X");
+	tree.insert("Y");
+	tree.insert("V");
+	tree.insert("T");
+	tree.insert("Z");
+	tree.insert("W");
+	//前序遍历
+	cout << "preorder traversal:" << endl;
+	tree.preorderTraversal();
+	//中序遍历
+	cout << endl;
+	cout << "inorder traversal:" << endl;
+	tree.inorderTraversal();
+	//后序遍历
+	cout << endl;
+	cout << "postorder traversal:" << endl;
+	tree.postorderTraversal();
+	//层次遍历
+	cout << endl;
+	cout << "level traversal:" << endl;
+	tree.levelTraversal();
+	cout << endl;
+	cout << "tree root is " <<tree.root()->data<< endl;
+	cout << endl;
+	cout << "remove tree root " << tree.root()->data << endl;
+	tree.remove(tree.root()->data);
+	cout << "now tree root is " << tree.root()->data << endl;
+	cout << "inorder traversal:" << endl;
+	tree.inorderTraversal();
+	cout << endl;
+	cout << "remove o "  << endl;
+	tree.remove("o");
+	cout << "now tree root is " << tree.root()->data << endl;
+	cout << "inorder traversal:" << endl;
+	tree.inorderTraversal();
+	cout << endl;
+	cout << "remove o " << endl;
+	tree.remove("o");
+	cout << "now tree root is " << tree.root()->data << endl;
+	cout << "inorder traversal:" << endl;
+	tree.inorderTraversal();
+	cout << endl;
+	cout << "remove d " << endl;
+	tree.remove("d");
+	cout << "now tree root is " << tree.root()->data << endl;
+	cout << "inorder traversal:" << endl;
+	tree.inorderTraversal();
+	cout << endl;
+	cout << endl; cout << endl; cout << endl;
+	//构造函数和移动构造函数测试
+	cout <<"copy constructor tree:"<< endl;
+	AVL<string>t=tree;
+	cout << "new tree root is " << t.root()->data << endl;
+	cout << "inorder traversal:" << endl;
+	t.inorderTraversal();
+	cout << endl;
+	cout << endl;
+	cout << "move constructor tree:" << endl;
+	AVL<string>t1 =std::move(tree);
+	cout << "new tree root is " << t1.root()->data << endl;
+	cout << "inorder traversal:" << endl;
+	t1.inorderTraversal();
+	cout << endl; cout << endl;
+	cout << "inorder traversal old tree:" << endl;
+	cout << endl;
+	tree.inorderTraversal();
+}
+//BST功能测试
+void testBST() {
+	BinSearchTree <int>tree;
+	tree.insertAsRoot(16);
+	BinTreeNode<int> *left = tree.insertAsLeft(tree.root(), 10);
+	BinTreeNode<int> *right = tree.insertAsRight(tree.root(), 25);
+	right = tree.insertAsLeft(right,19);
+	tree.insertAsRight(right, 22);
+	tree.insertAsLeft(right, 17);
+	right = tree.insertAsRight(left,11);
+	right = tree.insertAsRight(right,15);
+	tree.insertAsLeft(right, 13);
+	left= tree.insertAsLeft(left, 5);
+	tree.insertAsRight(left, 8);
+	left = tree.insertAsLeft(left,2);
+	tree.insertAsRight(left, 4);
+	right= tree.insertAsRight(tree.root()->right, 28);
+	tree.insertAsLeft(right, 27);
+	right=tree.insertAsRight(right, 37);
+	tree.insertAsLeft(right,33);
+	//中序遍历
+	cout << "looking here,inorder traversal result:" << endl;
+	tree.inorderTraversal(InorderTraversal<int>());
+	cout << endl;
+	
+	cout <<"insert:20 9 1 12 18"<< endl;
+	tree.insert(20); tree.insert(9); tree.insert(12); tree.insert(1); tree.insert(18);
+	cout << "looking here,inorder traversal result:" << endl;
+	tree.inorderTraversal(InorderTraversal<int>());
+	cout << endl;
+	cout << "insert:16 16 2 16 27 37 37 11" << endl;
+	tree.insert(16); tree.insert(16); tree.insert(2); tree.insert(16); tree.insert(27); tree.insert(37); tree.insert(37); tree.insert(11);
+	cout << "looking here,inorder traversal result:" << endl;
+	tree.inorderTraversal(InorderTraversal<int>()); 
+	cout << endl;
+	cout << "remove:16 16 2 16 27 37 37 11" << endl;
+	tree.remove(16); tree.remove(16); tree.remove(2); tree.remove(16); tree.remove(27); tree.remove(37); tree.remove(37); tree.remove(11);
+	cout << "looking here,inorder traversal result:" << endl;
+	tree.inorderTraversal(InorderTraversal<int>());
+	cout << endl;
+	cout << "remove:20 9 1 12 18" << endl;
+	tree.remove(20); tree.remove(9); tree.remove(1); tree.remove(12); tree.remove(18);
+	cout << "looking here,inorder traversal result:" << endl;
+	tree.inorderTraversal(InorderTraversal<int>());
+
+}
+void testTraversal() {
+	BinTree<string>tree;
+	tree.insertAsRoot("i");
+	BinTreeNode<string> *left= tree.insertAsLeft(tree.root(),"d");
+	BinTreeNode<string> *right =tree.insertAsLeft(left, "c");
+	right=tree.insertAsLeft(right, "a");
+	tree.insertAsRight(right, "b");
+	right=tree.insertAsRight(left, "h");
+	left=tree.insertAsLeft(right, "f");
+	tree.insertAsRight(left, "g");
+	tree.insertAsLeft(left, "e");
+	right=tree.insertAsRight(tree.root(), "l");
+	left=tree.insertAsLeft(right, "k");
+	tree.insertAsLeft(left, "j");	
+	right=tree.insertAsRight(right, "n");
+	tree.insertAsLeft(right, "m");
+	right = tree.insertAsRight(right, "p");
+	 tree.insertAsLeft(right, "o");
+	 //树层次排序为:i   d l   c h k n   a f j m p   b e g o  
+	//前序遍历
+	 cout << "preorder traversal:" << endl;
+	 tree.preorderTraversal();
+	 //中序遍历
+	 cout << endl;
+	 cout << "inorder traversal:" << endl;
+	 tree.inorderTraversal();
+	 //后序遍历
+	 cout << endl;
+	 cout << "postorder traversal:" << endl;
+	 tree.postorderTraversal();
+	 //层次遍历
+	 cout << endl;
+	 cout << "level traversal:" << endl;
+	 tree.levelTraversal();
+}
+
+void testBTN() {
+	BinTreeNode<int> root;
+	root.data = 3;
+	BinTreeNode<int>*p= root.insertAsLeft(1);
+	p->insertAsLeft(0);
+	p->insertAsRight(2);
+	p = root.insertAsRight(5);
+	p->insertAsLeft(4);
+	p = p->insertAsRight(6);
+	p->insertAsRight(7);
+	cout << "root size:"<<root.size() << endl;
+	cout << "left child  size:" << root.left->size()<< endl;
+	cout << "right child  size:" << root.right->size() << endl;
+	cout << "right child's right child size:" << p->size() << endl;
+	cout <<"zig and zag test of new binary search tree:"<< endl;
+
+
+	BinTree<string>tree;
+	tree.insertAsRoot("i");
+	BinTreeNode<string> *left = tree.insertAsLeft(tree.root(), "d");
+	BinTreeNode<string> *right = tree.insertAsLeft(left, "c");
+	right = tree.insertAsLeft(right, "a");
+	tree.insertAsRight(right, "b");
+	right = tree.insertAsRight(left, "h");
+	left = tree.insertAsLeft(right, "f");
+	tree.insertAsRight(left, "g");
+	tree.insertAsLeft(left, "e");
+	right = tree.insertAsRight(tree.root(), "l");
+	left = tree.insertAsLeft(right, "k");
+	tree.insertAsLeft(left, "j");
+	right = tree.insertAsRight(right, "n");
+	tree.insertAsLeft(right, "m");
+	right = tree.insertAsRight(right, "p");
+	tree.insertAsLeft(right, "o");
+	cout << "current level traversal:" << endl;
+	tree.levelTraversal();
+	cout << endl;
+	cout << "current inorder traversal:" << endl;
+	tree.inorderTraversal();
+	cout << endl;
+	cout << "zig(i):" << endl;
+	tree.root()->zig();
+	cout << "now level traversal:" << endl;
+	tree.levelTraversal();
+	cout << endl;
+	cout << "now inorder traversal:" << endl;
+	tree.inorderTraversal();
+	cout << endl;
+	cout << "zag(n):" << endl;
+	tree.root()->right->right->zag();
+	cout << "now level traversal:" << endl;
+	tree.levelTraversal();
+	cout << endl;
+	cout << "now inorder traversal:" << endl;
+	tree.inorderTraversal();
+	cout << endl;
+
+	}
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门提示: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
